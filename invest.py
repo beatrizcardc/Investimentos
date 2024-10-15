735da4ecd71313cf02aab78a28e004027e001d38
Beatriz Cardoso Cunha <72693632+beatrizcardc@users.noreply.github.com>
2024-10-15 17:44:03 -0300

    Update invest.py

--- invest.py
+++ invest.py
@@ -10,15 +10,15 @@ st.markdown("""
     <style>
         /* Cor de fundo geral da página */
         body {
-            background-color: #3A424B;
+            background-color: #202A30; 
         }
         /* Ajuste para o container principal de Streamlit */
         .stApp {
-            background-color: #3A424B;
+            background-color: #202A30;
         }
         /* Cor de fundo da barra lateral */
         .css-1d391kg {
-            background-color: #C0C0C0;
+            background-color: #3A424B;
         }
         /* Ajuste do background para caixas de seleção, sliders, e inputs */
         .stSlider .st-b9, .stNumberInput input, .stSelectbox select, .stTextInput input {
